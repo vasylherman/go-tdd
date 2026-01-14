abe49de010a6bf18b5becbfd9d0f136c3d12be74
Vasyl Herman 🌥 <vasilgerman@gmail.com>
2026-01-14 20:19:20 +0200

    init

--- integers/add_test.go
+++ integers/add_test.go
@@ -1,13 +1,12 @@
-package integers_test
+package integers
 
 import (
 	"fmt"
-	"github.comvasylherman/go-tdd/integers"
 	"testing"
 )
 
 func TestAdd(t *testing.T) {
-	sum := integers.Add(2, 2)
+	sum := Add(2, 2)
 	expected := 4
 	if sum != expected {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", sum, expected)
@@ -15,13 +14,13 @@ func TestAdd(t *testing.T) {
 }
 
 func ExampleAdd() {
-	sum := integers.Add(2, 2)
+	sum := Add(2, 2)
 	fmt.Println(sum)
 	// Output: 4
 }
 
 func ExampleAdd_second() {
-	sum := integers.Add(3, 2)
+	sum := Add(3, 2)
 	fmt.Println(sum)
 	// Output:
 	// 5
