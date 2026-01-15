bf739bd28e16d670776d2e884edf401da271518e
Vasyl Herman 🌥 <vasilgerman@gmail.com>
2026-01-15 15:03:34 +0200

    init iteration add bench

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,9 +1,19 @@
 package iteration
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleRepeat() {
 	repeated := Repeat("a")
 	fmt.Println(repeated)
 	// Output: aaaaa
+}
+
+// Run go test ./iteration -bench=. to execute BenchmarkRepeat
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a")
+	}
 }
