70d887f9b2c77fd1daa5e8325fe586454e012535
Vasyl Herman 🌥 <vasilgerman@gmail.com>
2026-01-16 14:21:11 +0200

    arrays: init

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,15 +5,24 @@ import (
 	"testing"
 )
 
+func TestSumArray(t *testing.T) {
+	numbers := [...]int{1, 2, 3}
+	got := SumArray(numbers)
+	want := 6
+	if want != got {
+		fmt.Printf("want %d, got %d, given %#v", want, got, numbers)
+	}
+}
+
 func ExampleSumArray() {
-	numbers := [3]int{1, 2, 3}
+	numbers := [...]int{1, 2, 3}
 	sum := SumArray(numbers)
 	fmt.Println(sum)
 	// Output: 6
 }
 
 func BenchmarkSum(b *testing.B) {
-	numbers := [3]int{1, 2, 3}
+	numbers := [...]int{1, 2, 3}
 	for b.Loop() {
 		SumArray(numbers)
 	}
